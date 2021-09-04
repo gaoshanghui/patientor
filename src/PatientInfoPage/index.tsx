2637887096f01a2ebedf05dfefca57edac696684
Gao Shanghui <shanghui0109@gmail.com>
2021-09-04 09:55:53 +0900

    Add EntryDetails component

--- src/PatientInfoPage/index.tsx
+++ src/PatientInfoPage/index.tsx
@@ -2,17 +2,58 @@ import axios from 'axios';
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
 
-import { Patient } from '../types';
+import { Patient, Entry } from '../types';
 
 import { apiBaseUrl } from '../constants';
 
 import { useStateValue, setPatient, setDiagnoses } from '../state';
 
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+// Component: Patient's entry detail
+const EntryDetails = ({ entry }: { entry: Entry }) => {
+  switch (entry.type) {
+    case 'HealthCheck':
+      return (
+        <div>
+          <p>{entry.date}</p>
+          <p>{entry.description}</p>
+          <p>Health Check Rating: {entry.healthCheckRating}</p>
+        </div>
+      );
+    case 'Hospital':
+      return (
+        <div>
+          <p>{entry.date}</p>
+          <p>{entry.description}</p>
+          <p>
+            Discharge: {entry.discharge.date} {entry.discharge.criteria}
+          </p>
+        </div>
+      );
+    case 'OccupationalHealthcare':
+      return (
+        <div>
+          <p>{entry.date}</p>
+          <p>{entry.description}</p>
+          <p>
+            Sick Leave: {entry.sickLeave?.startDate} {entry.sickLeave?.endDate}
+          </p>
+        </div>
+      );
+    default:
+      return assertNever(entry);
+  }
+};
+
 const PatientInfoPage = () => {
   const { id } = useParams<{ id: string }>();
 
-  const [{ patient, diagnoses }, dispatch] = useStateValue();
-  console.log('diagnoses: ', diagnoses);
+  const [{ patient }, dispatch] = useStateValue();
 
   useEffect(() => {
     const fetchPatientInformation = async () => {
@@ -22,7 +63,7 @@ const PatientInfoPage = () => {
         );
         dispatch(setPatient(response.data));
       } catch (error) {
-        console.log(error.message);
+        console.log(error);
       }
     };
 
@@ -31,7 +72,7 @@ const PatientInfoPage = () => {
         const response = await axios.get(`${apiBaseUrl}/diagnoses/`);
         dispatch(setDiagnoses(response.data));
       } catch (error) {
-        console.log(error.message);
+        console.log(error);
       }
     };
 
@@ -59,21 +100,22 @@ const PatientInfoPage = () => {
         <h3>entries</h3>
         {patient.entries.map((entry) => {
           return (
-            <div key={entry.id}>
-              <p>{entry.date}</p>
-              <p>{entry.description}</p>
-              <ul>
-                {entry.diagnosisCodes?.map((code) => (
-                  <li key={code}>
-                    {code}{' '}
-                    {
-                      diagnoses?.find((diagnosis) => diagnosis.code === code)
-                        ?.name
-                    }
-                  </li>
-                ))}
-              </ul>
-            </div>
+            // <div key={entry.id}>
+            //   <p>{entry.date}</p>
+            //   <p>{entry.description}</p>
+            //   <ul>
+            //     {entry.diagnosisCodes?.map((code) => (
+            //       <li key={code}>
+            //         {code}{' '}
+            //         {
+            //           diagnoses?.find((diagnosis) => diagnosis.code === code)
+            //             ?.name
+            //         }
+            //       </li>
+            //     ))}
+            //   </ul>
+            // </div>
+            <EntryDetails key={entry.id} entry={entry} />
           );
         })}
       </div>
